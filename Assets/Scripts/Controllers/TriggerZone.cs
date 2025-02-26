ff16679c1382763e15dc0758f0cbcb81386b6469
Fachri Ahmad S <fachrias2107@gmail.com>
2025-02-26 11:15:45 +0700

    right + left walk

--- Assets/Scripts/Controllers/TriggerZone.cs
+++ Assets/Scripts/Controllers/TriggerZone.cs
@@ -5,16 +5,35 @@ using UnityEngine.Events;
 
 public class TriggerZone : MonoBehaviour
 {
+    public GameObject keyObject;
+    public Transform viewObject;
+    
     public bool oneShot = false;
+    public bool showKey = false;
+    public Vector3 offset = new Vector3(-0.5f, 1.5f, 0f);
     private bool alreadyEntered = false;
     private bool alreadyExited = false;
 
     public string collisionTag;
-
+    
     public UnityEvent onTriggerEnter;
     public UnityEvent onTriggerExit;
 
+    void Update()
+    {
+         if (showKey)
+        {
+            keyObject.SetActive(true);
+            // Update posisi bubble chat agar selalu di kiri atas NPC
+            transform.position = viewObject.position + offset;
+        }else if (!showKey)
+        {
+            keyObject.SetActive(false);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision){
+        showKey = true;
         if (alreadyEntered)
             return;
 
@@ -28,6 +47,7 @@ public class TriggerZone : MonoBehaviour
     }
 
     private void OnTriggerExit2D(Collider2D collision){
+        showKey = false;
         if (alreadyExited)
             return;
 
@@ -39,4 +59,6 @@ public class TriggerZone : MonoBehaviour
         if (oneShot)
             alreadyExited = true;
     }
+    
+    
 }
